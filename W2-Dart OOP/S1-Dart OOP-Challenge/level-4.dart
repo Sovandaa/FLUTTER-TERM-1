33c2340c7497439d50d7b92aa22dcdcfafd9a516
Sovanda <bansovanda.net@gmail.com>
2024-10-16 09:24:49 +0700

    updated

--- W2-Dart OOP/S1-Dart OOP-Challenge/level-4.dart
+++ W2-Dart OOP/S1-Dart OOP-Challenge/level-4.dart
@@ -8,22 +8,25 @@ class Distance {
   Distance.meters(this.value);
   Distance.kms(this.value);
 
-  double get cms {
-    return value * 0.01;
+  //add 2 distances use operator overloading
+  Distance operator +(Distance other) {
+    return Distance(value + other.value);
   }
 
-  double get meters {
-    return value;
-  }
-
-  double get kms {
-    return value * 1000;
-  }
+  // double get cms {
+  //   return value * 0.01;
+  // }
+  // double get meters {
+  //   return value;
+  // }
+  // double get kms {
+  //   return value / 1000;
+  // }
 }
 
 void main() {
-  Distance d1 = Distance.cms(3.4);
-  Distance d2 = Distance(1000);
-  
+  Distance d1 = Distance.kms(3.4);
+  Distance d2 = Distance.meters(1000);
+
   //print((d1 + d2).kms);
 }
