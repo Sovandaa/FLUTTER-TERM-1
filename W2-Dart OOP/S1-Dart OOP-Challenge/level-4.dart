e62c906a9c551231475faf2ef292e8f4d298c96b
Sovanda <bansovanda.net@gmail.com>
2024-10-16 09:50:02 +0700

    update convert to other units

--- W2-Dart OOP/S1-Dart OOP-Challenge/level-4.dart
+++ W2-Dart OOP/S1-Dart OOP-Challenge/level-4.dart
@@ -3,30 +3,34 @@ class Distance {
 
   Distance(this.value); // main constructor
 
-  // named constructor
-  Distance.cms(this.value);
+  // named constructor (convert to meters unit)
+  Distance.cms(double cms): value = cms * 0.01;
   Distance.meters(this.value);
-  Distance.kms(this.value);
+  Distance.kms(double kms): value = kms * 1000;
 
   //add 2 distances use operator overloading
-  Distance operator +(Distance other) {
+  Distance operator + (Distance other) {
     return Distance(value + other.value);
   }
 
-  // double get cms {
-  //   return value * 0.01;
-  // }
-  // double get meters {
-  //   return value;
-  // }
-  // double get kms {
-  //   return value / 1000;
-  // }
+  // getter (convert meters to other units)
+  double get cms {
+    return value * 100;
+  }
+  double get meters {
+    return value;
+  }
+  double get kms {
+    return value / 1000;
+  }
 }
 
 void main() {
-  Distance d1 = Distance.kms(3.4);
-  Distance d2 = Distance.meters(1000);
+  Distance d1 = Distance.kms(3.4); // 3400 m
+  Distance d2 = Distance.meters(1000); // 1000 m
 
-  //print((d1 + d2).kms);
+  print((d1 + d2).kms);
+  print((d1 + d2).meters);
+  print((d1 + d2).cms);
+
 }
