03152b557565339bdb329d1c9a9d447bd3b04042
Sovanda <bansovanda.net@gmail.com>
2024-11-02 13:35:18 +0700

    update code

--- lib/W4-S1-Practice/EX-2/main.dart
+++ lib/W4-S1-Practice/EX-2/main.dart
@@ -1,43 +1,47 @@
 // The Products
 import 'package:flutter/material.dart';
 
-String dartImage = "assets/W4-S1-images/dart.png";
-String firebaseImage = "assets/W4-S1-images/firebase.png";
-String flutterImage = "assets/W4-S1-images/flutter.png";
+// String dartImage = "assets/W4-S1-images/dart.png";
+// String firebaseImage = "assets/W4-S1-images/firebase.png";
+// String flutterImage = "assets/W4-S1-images/flutter.png";
+
+enum Product {
+  Dart(
+      title: "Dart",
+      description: "the best object language",
+      image: "assets/W4-S1-images/dart.png"),
+  Flutter(
+      title: "Flutter",
+      description: "the best mobile widget library",
+      image: "assets/W4-S1-images/flutter.png"),
+  Firebase(
+      title: "Firebase",
+      description: "the best cloud database",
+      image: "assets/W4-S1-images/firebase.png");
+
+  final String title;
+  final String description;
+  final String image;
+
+  const Product(
+      {required this.title, required this.description, required this.image});
+}
 
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
       backgroundColor: Colors.blue,
-      appBar:  AppBar(
+      appBar: AppBar(
         title: const Text("Products"),
       ),
-      body: Padding(
+      body: const Padding(
         padding: const EdgeInsets.all(20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-           
-            Card(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Padding(padding: EdgeInsets.all(5)),
-                  SizedBox(
-                    width: 100,
-                      height: 70,
-                      child: Image.asset(
-                        dartImage,
-                        fit: BoxFit.contain,
-                      ),
-                    ),
-                  SizedBox(height: 10.0),
-                  Text("Dart"),
-                  SizedBox(height: 8.0),
-                  Text("The best object language"),
-                ],
-              ),
-            )
+            ProductCard(product: Product.Dart),
+            ProductCard(product: Product.Flutter),
+            ProductCard(product: Product.Firebase),
           ],
         ),
       ),
@@ -45,3 +49,46 @@ void main() {
   ));
 }
 
+class ProductCard extends StatelessWidget {
+  // const ProductCard({
+  //   super.key,
+  // });
+  final Product product;
+  const ProductCard({required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          crossAxisAlignment:
+              CrossAxisAlignment.start, // Aligns items to the left
+          children: [
+            Image.asset(
+              product.image,
+              width: 80,
+              height: 80,
+            ),
+            const SizedBox(height: 6.0),
+            Text(
+              product.title,
+              style: const TextStyle(
+                fontSize: 22,
+              ),
+            ),
+            const SizedBox(height: 8.0),
+            Text(
+              product.description,
+              style: const TextStyle(
+                fontSize: 14,
+                color: Colors.grey,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/W4-S1-Practice/Ex-3/main.dart
+++ lib/W4-S1-Practice/Ex-3/main.dart
@@ -2,13 +2,44 @@
 
 import 'package:flutter/material.dart';
 
+// enum ButtonType {
+//   Color color;
+
+//   const ButtonType ({this.color});
+  
+// }
+
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
         title: Text("Custom Buttons"),
       ),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextButton(
+                // style: TextButton.styleFrom(
+                //   Primary: Colors.blue,
+                //   Secondary: Colors.green,
+                //   Disabled: Colors.grey
+                // ),
+                onPressed: () {}, 
+                // Icon(
+                //   Icons.check,
+                //   color: Colors.grey,
+                //   size: 24.0,
+                //   ),
+                // const SizedBox(width: 20.0),
+                child:const Text("Submit"),
+              )
+            ],
+          ),
+        ),
+      ),
     ),
   ));
 }
-
