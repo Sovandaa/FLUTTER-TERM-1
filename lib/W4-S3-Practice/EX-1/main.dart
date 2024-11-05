0483f155e9c8775080c6f0c540b2aa82903cce4f
Sovanda <bansovanda.net@gmail.com>
2024-11-06 00:06:30 +0700

    commit w4-s3

--- lib/W4-S3-Practice/EX-1/main.dart
+++ lib/W4-S3-Practice/EX-1/main.dart
@@ -1,12 +1,59 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Statefull widget button"),
-      ),
-      body: Center(),
-    ),
-  ));
+class CustomButton extends StatefulWidget {
+  const CustomButton({super.key});
+
+  @override
+  State<CustomButton> createState() => _CustomButtonState();
 }
+
+class _CustomButtonState extends State<CustomButton> {
+  bool isSelected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(5),
+      child: SizedBox(
+        width: 350,
+        height: 80,
+        child: ElevatedButton(
+            onPressed: () {
+              setState(() { // switches between true and false (call when press btn)
+                isSelected = !isSelected;
+              });
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: isSelected ? Colors.blue : Colors.lightBlue[50],
+            ),
+            child: Center(
+              child: Text(
+                isSelected ? 'Selected' : 'Not Selected',
+                style: const TextStyle(
+                  fontSize: 24,
+                ),
+              ),
+            )),
+      ),
+    );
+  }
+}
+
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Statefull widget - Button"),
+        ),
+        body: const Center(
+          child: Column(
+            children: [
+              CustomButton(),
+              CustomButton(),
+              CustomButton(),
+              CustomButton(),
+            ],
+          )
+          
+        ),
+      ),
+    ));
