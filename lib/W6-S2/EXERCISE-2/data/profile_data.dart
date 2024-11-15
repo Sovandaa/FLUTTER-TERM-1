85de13b308ceca8dde073658fe1911bc0c9080d5
Sovanda <bansovanda.net@gmail.com>
2024-11-15 11:38:17 +0700

    commit

---
+++ lib/W6-S2/EXERCISE-2/data/profile_data.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_workspace/W6-S2/EXERCISE-2/model/profile_tile_model.dart';
+
+ProfileData myProfile = ProfileData(
+    name: "Sovanda",
+    position: "Flutter Developer",
+    avatarUrl: 'assets/W6-S2/woman.png',
+    tiles: [
+      TileData(
+          icon: Icons.phone, title: "Phone Number", value: "+123 456 7890"),
+      TileData(
+          icon: Icons.location_on, title: "Address", value: "123 Cambodia"),
+      TileData(
+        icon: Icons.email, title: "Mail", value: "sovanda.ban@student.cadt.edu"
+        ),
+      TileData(
+        icon: Icons.school, title: "Major", value: "Computer Science")
+    ]);
